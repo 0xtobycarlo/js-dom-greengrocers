c7e550b312d185a27e8e57bcec1e50867f96a81d
Toby Carlo <tobycarlo.c@gmail.com>
2022-04-22 16:51:54 +0100

    renderStore() and filters

--- index.js
+++ index.js
@@ -68,3 +68,52 @@ function clear() {
   storeList.innerHTML = "";
   cartList.innerHTML = "";
 }
+
+function renderStore() {
+  for (const item of state.items) {
+    const storeItem = document.createElement("li");
+
+    const storeDiv = document.createElement("div");
+    storeDiv.setAttribute("class", "store--item-icon");
+
+    const storeImg = document.createElement("img");
+    storeImg.setAttribute("src", `assets/icons/${item.id}.svg`);
+
+    const storeButton = document.createElement("button");
+    storeButton.innerText = "ADD TO CART";
+    storeButton.addEventListener("click", function () {
+      const alreadyInCart = state.cart.find((i) => i.item === item);
+      if (alreadyInCart !== undefined) {
+        alreadyInCart.quantity++;
+      } else {
+        state.cart.push({
+          quantity: 1,
+          item: item,
+        });
+      }
+      console.log("Items in cart", state.cart);
+      render();
+    });
+
+    const fruitFilter = document.querySelector(".show-me-fruit");
+    fruitFilter.addEventListener("click", function () {
+      state.items = state.items.filter((item) => item.type === "fruit");
+      console.log("Clicking fruit button");
+      render();
+      return;
+    });
+
+    const vegFilter = document.querySelector(".show-me-veg");
+    vegFilter.addEventListener("click", function () {
+      state.items = state.items.filter((item) => item.type === "vegetable");
+      render();
+      return;
+    });
+
+    const noFilter = document.querySelector(".show-me-it-all");
+    noFilter.addEventListener("click", function () {
+      state.items = state.items;
+      render();
+    });
+  }
+}
